5472e2796806f0488d382788c68cc6ded02ee48a
DaronP <jandresperez1@yahoo.com>
2017-05-12 12:53:27 -0600

    first commit

---
+++ Assets/Scripts/Win.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Win : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider col){
+
+		if (col.gameObject.tag == "Player") {
+			Cursor.visible = true;
+			Cursor.lockState = CursorLockMode.None;
+			Screen.lockCursor = false;
+			
+			Application.LoadLevel(1);
+
+
+		}
+
+	}
+}
